ce4db6e953b8aea5fc29509d04c74843ab5b80e6
Marisa Ayala <marisa.ayala118@gmail.com>
2025-12-23 00:27:58 -0300

    Clase 11 - Forms - Admin - Herencia Templates

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from hospital.models import *
 
-# Register your models here.
+#admin.site.register(DepartamentoMedico)
+
+@admin.register(DepartamentoMedico)
+class DepartamentoMedico(admin.ModelAdmin):
+    # columnas visibles en la lista del modelo
+    list_display = ("nombre", "nro_departamento", "email_dpto", "fecha_de_creacion")
+    # columnas con enlaces clickeables para entrar en el detalle
+    list_display_links = ("nombre",)	#con coma al final xq es una tupla
+    # campos por los que se pueden buscar
+    search_fields = ("nro_departamento",)
+    # filtros laterales
+    list_filter = ("fecha_de_creacion",)
+    # orden por defecto
+    ordering = ("nro_departamento", "nombre")
+    # campos de solo lectura
+    readonly_fields = ("fecha_de_creacion",)
--- hospital/views.py
+++ hospital/views.py
@@ -1,15 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from hospital.models import DepartamentoMedico
+from hospital.forms import DepartamentoMedicoForm
 
 def home(request):
     return render(request, "hospital/index.html")
 
 
 def listar_departamentos(request):
+    nombre = request.GET.get("nombre")
     departamentos_query = DepartamentoMedico.objects.all() #list(QuerySet[Depto, ...., Depto, ....]) 
+    if nombre: # si nombre NO ES None / '' / etc
+        departamentos_query = DepartamentoMedico.objects.filter(
+            nombre__icontains=nombre
+        )
     contexto = {
         "departamentos": departamentos_query
     }
     return render(request, "hospital/departamentos_medicos.html", contexto)
 
+#GET = pedir infor
+#POST - crear / editar info
+#PUT - actualizar info
+#DELETE - eliminar info
 
+
+def crear_departamento(request):
+    if request.method == "POST":
+        form = DepartamentoMedicoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listar_departamentos")
+    else:
+        form = DepartamentoMedicoForm()
+    
+    return render(request, 'hospital/crear_departamento.html', {'form': form})
+
+
+def ver_departamento(request, pk):
+    departamento = get_object_or_404(DepartamentoMedico, pk=pk)
+    context = {
+        "departamento": departamento
+    }
+    
+    return render(request, 'hospital/ver_departamento.html', context)
